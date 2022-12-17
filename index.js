9f0eff83a9163327f0707fa8ae990bfd6514d850
rabhamonojoy <mjykoch177@gmail.com>
2022-12-17 16:44:56 +0530

    blog

---
+++ index.js
@@ -0,0 +1,12 @@
+let Time=document.getElementById("Time")
+Time.addEventListener("click",function(){
+    window.location.assign("time.html")
+})
+let Project=document.getElementById("Time")
+Project.addEventListener("click",function(){
+
+})
+let Team=document.getElementById("Time")
+Team.addEventListener("click",function(){
+
+})
