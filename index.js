023f81a9fcb472574b1cee107d917edf6a0dc5ad
rabhamonojoy <mjykoch177@gmail.com>
2022-12-18 23:48:30 +0530

    pricing

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ let Team=document.getElementById("Time")
 Team.addEventListener("click",function(){
 
 })
+let Blog=document.querySelector("#Blog")
+Blog.addEventListener("click",()=>{
+   window.location.assign("blog.html")
+})
+let Signup=document.getElementById("Signup")
+Signup.addEventListener("click",()=>{
+   window.location.assign("signup.html")
+})
+document.getElementById("Login").addEventListener("click",()=>{
+   window.location.assign("Login.html")
+})
+document.getElementById("Pricing").addEventListener("click",()=>{
+   window.location.assign("pricing.html")
+})
+document.getElementById("twiiter").addEventListener("click",()=>{
+   window.location.assign("https://twitter.com/TMetric_Timer")
+})
+document.getElementById("linkendin").addEventListener("click",()=>{
+   window.location.assign("https://www.linkedin.com/showcase/tmetric/")
+})
